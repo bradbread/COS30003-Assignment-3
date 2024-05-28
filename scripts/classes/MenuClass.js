f999b5097771937c25800717c780ef275fe88a92
9iftttt <chonnikarn.gift@gmail.com>
2024-05-28 11:04:30 +1000

    Menu Class Implememt

---
+++ scripts/classes/MenuClass.js
@@ -0,0 +1,10 @@
+// menu.js
+export class Menu {
+    constructor(id, name, description, price, image) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,51 @@
-const { createApp } = Vue
-const app = createApp()
+// main.js
+import { Menu } from './classes/MenuClass.js';
 
-app.component('app-main', // indicating the component tag
-  {
-    // define the template for the component
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            menu: []
+        };
+    },
+    created() {
+        this.fetchMenu();
+    },
+    methods: {
+        fetchMenu() {
+            fetch('data/menu.json')
+                .then(response => response.json())
+                .then(data => {
+                    this.menu = data.map(item => new Menu(item.id, item.name, item.description, item.price, item.image));
+                })
+                .catch(error => console.error('Error fetching menu:', error));
+        }
+    }
+});
+
+app.component('app-main', {
+    props: ['menu'],
     template: `
     <div>
-		<h1>Relaxing Koala</h1>
-  	</div>
-    `,	
+        <h1>Relaxing Koala</h1>
+        <div v-if="menu && menu.length" class="row">
+            <div class="col-md-4" v-for="item in menu" :key="item.id">
+                <div class="card mb-4">
+                    <img :src="item.image" class="card-img-top" :alt="item.name">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ item.name }}</h5>
+                        <p class="card-text">{{ item.description }}</p>
+                        <p class="card-text"><strong>Price:</strong> {{ item.price }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div v-else>
+            <p>Loading menu...</p>
+        </div>
+    </div>
+    `
+});
 
-  });
-
-app.mount('#app')
+app.mount('#app');
